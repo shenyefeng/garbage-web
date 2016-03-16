c8f7dda53460b9f1788a03172a8a83251c429efb
yefeng.shen <yefeng.shen@allianz.cn>
2016-03-16 19:34:04 +0800

    add session cache

---
+++ src/main/java/me/anchora/garbage/filter/RedisSessionFilter.java
@@ -0,0 +1,90 @@
+package me.anchora.garbage.filter;
+
+import java.io.IOException;
+import java.net.URI;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.mchange.v2.ser.SerializableUtils;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+public class RedisSessionFilter implements Filter {
+	private static final Logger logger = LoggerFactory.getLogger(RedisSessionFilter.class);
+
+	private static ThreadLocal<HttpServletRequest> currentRequest = new ThreadLocal<HttpServletRequest>();
+	private static ThreadLocal<HttpServletResponse> currentResponse = new ThreadLocal<HttpServletResponse>();
+	private static ThreadLocal<RedisSessionFilter> currentFilter = new ThreadLocal<RedisSessionFilter>();
+
+	private boolean cacheFlag = false;
+	private JedisPool jedisPool;
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		try {
+			String flag = filterConfig.getInitParameter("cacheFlag");
+			logger.debug("RedisSessionFilter param: cacheFlag=" + flag);
+			cacheFlag = Boolean.valueOf(flag);
+			if (cacheFlag) {
+				GenericObjectPoolConfig config = new GenericObjectPoolConfig();
+				String uri = filterConfig.getInitParameter("uri");
+				logger.debug("RedisSessionFilter param: uri=" + uri);
+				jedisPool = new JedisPool(config, new URI(uri));
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		if (request instanceof HttpServletRequest && cacheFlag) {
+			currentFilter.set(this);
+			currentRequest.set((HttpServletRequest) request);
+			currentResponse.set((HttpServletResponse) response);
+			handle((HttpServletRequest) request, (HttpServletResponse) response, chain);
+			currentFilter.set(null);
+			currentRequest.set(null);
+			currentResponse.set(null);
+		} else {
+			chain.doFilter(request, response);
+		}
+	}
+
+	private void handle(final HttpServletRequest request, final HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		Jedis jedis = jedisPool.getResource();
+		String sessionId = request.getSession().getId();
+		if(jedis.exists(sessionId.getBytes())) {
+			try {
+				HttpSession s = (HttpSession)SerializableUtils.fromByteArray(jedis.get(sessionId.getBytes()));
+//				s.getServletContext().geta;
+			} catch (ClassNotFoundException e) {
+				throw new RuntimeException(e);
+			}
+		} else {
+			jedis.set(sessionId.getBytes(), SerializableUtils.toByteArray(request.getSession()));
+		}
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+		jedisPool.destroy();
+	}
+}
